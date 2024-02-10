d8d0856d00abcfdd880e82ebfba71168a4205b08
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-02-10 19:44:11 -0300

    Add Cost Explorer Spot

--- glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
+++ glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
@@ -41,8 +41,12 @@ MY_HASTA=$(date +%Y-%m-%dT%H:%M:%S)
 
 
 #---------------------
-# MY_SPOT=g4dn.2xlarge
-MY_SPOT=g5.2xlarge
+MY_SPOT=g4dn.2xlarge
+# MY_SPOT=g4dn.4xlarge
+# MY_SPOT=g5.2xlarge
+# MY_SPOT=g5.4xlarge
+
+
 
 REGIONS_CON_GPU_ENUSO=("eu-north-1" "us-east-1")
 REGIONS_CON_GPU_TENGO=("eu-north-1" "us-east-1"  "us-west-2" "af-south-1"  )
@@ -79,7 +83,24 @@ echo "REGIONS CON GPU CHEAP"
 getPrecio_xRegiones  ${REGIONS_CON_GPU_CHEAP[@]}
 
 
+echo "BEST g4dn.2xlarge - "
+echo "BEST g4  .2xlarge - "
+
+echo "BEST g5.2xlarge   - 0.206' (sa-east-1b)  - 0.4236 (eu-north-1b)
+
 #  RUN
 #
+# g4dn.2xlarge
+# g4dn.4xlarge
+#
+# g5  .2xlarge
+# g5  .4xlarge
+#  
+#  Filtro g[45][^ga].*\.[24]x?large
+#
+#  https://instances.vantage.sh/?min_vcpus=8&filter=g[45][^ga].*\.[24]x?large&selected=g4dn.2xlarge,g4dn.4xlarge,g5.2xlarge,g5.4xlarge
+#  https://instances.vantage.sh/?filter=g[45][^ga].*\.[24]x?large&region=ap-northeast-2&selected=g4dn.2xlarge,g4dn.4xlarge,g5.2xlarge,g5.4xlarge
+#
+
 #  ./cost_spots.sh > costs_g5.2xlarge
 
