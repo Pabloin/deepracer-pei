af70ae74debdfa5ffe060159d7f2a16c2fca9e96
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-02-10 13:09:02 -0300

    Add Cost Explorer Spot

--- glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
+++ glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
@@ -1,11 +1,44 @@
-# TODO:
+#!/bin/bash
+
 
 ## Script para saber en que region la instancia SPOT XX está mas barata ... 
 
+function getPriceSpots() {
 
-aws ec2 describe-spot-price-history   `
-     --instance-types g4dn.2xlarge     `
-     --product-descriptions "Linux/UNIX" `
-     --start-time 2024-01-26T07:08:09  `
-     --end-time   2024-01-27T08:09:10   `
-     --output text
+     aws ec2 describe-spot-price-history    \
+          --instance-types ${MY_SPOT}      \
+          --product-descriptions "Linux/UNIX"  \
+          --start-time ${MY_DESDE}   \
+          --end-time   ${MY_HASTA}    \
+          --output text  \
+          --profile racer1 \
+          --region ${MY_REGION}
+   
+}
+
+MY_DESDE=2024-02-09T01:00:09
+MY_HASTA=2024-02-10T23:59:00
+
+
+#---------------------
+MY_SPOT=g4dn.2xlarge
+
+# Europe (Milan)	               eu-south-1
+# Europe (Stockholm)	          eu-north-1
+# South America (São Paulo)	sa-east-1
+# Asia Pacific (Osaka)	     ap-northeast-3
+# Middle East (Bahrain)	     me-south-1
+# Middle East (Bahrain)	     me-south-1
+
+MY_REGIONS=("sa-east-1" "eu-north-1" "us-east-1")
+
+MY_SPOT=g4dn.2xlarge
+
+for region in "${MY_REGIONS[@]}"; do
+
+     MY_REGION=${region}
+     getPriceSpots
+
+done
+
+
