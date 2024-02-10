db5af08608fb3e319915f80af4ba4faa8d7e6380
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-02-10 13:44:33 -0300

    Add Cost Explorer Spot

--- glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
+++ glaciar/030_Scripts_DRFC/aws_scripts/cost_spots.sh
@@ -3,7 +3,7 @@
 
 ## Script para saber en que region la instancia SPOT XX está mas barata ... 
 
-function getPriceSpots() {
+function getSpotPrice_xRegion() {
 
      aws ec2 describe-spot-price-history    \
           --instance-types ${MY_SPOT}      \
@@ -16,32 +16,61 @@ function getPriceSpots() {
    
 }
 
-MY_DESDE=2024-02-09T01:00:09
-MY_HASTA=2024-02-10T23:59:00
+function getPrecio_xRegiones() {
+
+     echo ${1}
+     
+     for region in "${MY_REGIONS[@]}"; do
+
+          echo "---[Region ${region} ] ----------"
+          MY_REGION=${region}
+          getSpotPrice_xRegion
+
+     done
+   
+}
+
+# Ejemplo
+# MY_DESDE=2024-02-09T01:00:09
+# MY_HASTA=2024-02-10T23:59:00
 
 # 1 hora
-MY_DESDE=$(date +%Y-%m-%dT%H:%M:%S)
-MY_HASTA=2024-02-10T23:59:00
+MY_DESDE=$(date +%Y-%m-%dT%H:%M:%S --date='-2 hour')
+MY_HASTA=$(date +%Y-%m-%dT%H:%M:%S)
+
+
 
 #---------------------
 MY_SPOT=g4dn.2xlarge
 
-# Europe (Milan)	               eu-south-1
-# Europe (Stockholm)	          eu-north-1
+REGIONS_CON_GPU_ENUSO=("eu-north-1" "us-east-1")
+REGIONS_CON_GPU_TENGO=("eu-north-1" "us-east-1"  "us-west-2" "af-south-1"  )
+REGIONS_CON_GPU_CHEAP=("eu-south-1" "eu-north-1" "sa-east-1" "ap-northeast-3" "me-south-1" )
+
+# REGIONS_CON_GPU_ENUSO
+# - US East (N. Virginia)	us-east-1
+# - Europe (Stockholm)	     eu-north-1
+
+# REGIONS_CON_GPU_TENGO
+# - US East (N. Virginia)	us-east-1
+# - US West (Oregon)	     us-west-2
+# - Africa (Cape Town)	     af-south-1
+# - Europe (Stockholm)	     eu-north-1
+
+# REGIONS_CON_GPU_CHEAP
+# Europe (Milan)         	eu-south-1
+# Europe (Stockholm)	     eu-north-1
 # South America (São Paulo)	sa-east-1
 # Asia Pacific (Osaka)	     ap-northeast-3
 # Middle East (Bahrain)	     me-south-1
-# Middle East (Bahrain)	     me-south-1
 
-MY_REGIONS=("sa-east-1" "eu-north-1" "us-east-1")
 
-MY_SPOT=g4dn.2xlarge
+MY_REGIONS=${REGIONS_CON_GPU_ENUSO}
+getPrecio_xRegiones "REGIONS_CON_GPU_ENUSO"
 
-for region in "${MY_REGIONS[@]}"; do
+MY_REGIONS=${REGIONS_CON_GPU_TENGO}
+getPrecio_xRegiones "REGIONS_CON_GPU_TENGO"
 
-     MY_REGION=${region}
-     getPriceSpots
-
-done
-
+MY_REGIONS=${REGIONS_CON_GPU_CHEAP}
+getPrecio_xRegiones "REGIONS_CON_GPU_CHEAP"
 
