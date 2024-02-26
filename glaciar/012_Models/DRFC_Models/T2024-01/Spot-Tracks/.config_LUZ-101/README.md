f7ecb007af8bb729b26d539b0325842dd9b58483
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-02-26 04:00:32 -0300

    Add Bat Scripts

--- glaciar/012_Models/DRFC_Models/T2024-01/Spot-Tracks/.config_LUZ-101/README.md
+++ glaciar/012_Models/DRFC_Models/T2024-01/Spot-Tracks/.config_LUZ-101/README.md
@@ -105,3 +105,15 @@ ____________________
 Nota 2024-02-26
 
     Vamos con 101a
+
+
+PREFIX='model-name'   # Name of the model, without trailing '/'
+BUCKET='bucket'       # Bucket name is default 'bucket' when training locally
+PROFILE=None          # The credentials profile in .aws - 'minio' for local training
+S3_ENDPOINT_URL=None  # Endpoint URL: None for AWS S3, 'http://minio:9000' for local training
+
+PREFIX='Spot-Tracks/LUZ-101a'
+BUCKET='bucket-models-2024-01'
+S3_ENDPOINT_URL='http://minio:9000'
+
+AmericasGeneratedInclStart
--- glaciar/030_Scripts_DRFC/aws_scripts/bestion.bat
+++ glaciar/030_Scripts_DRFC/aws_scripts/bestion.bat
@@ -1,24 +1,41 @@
 @echo off
 
+REM for %%x in (glaciar racer1 racer2 racer3) do (
+
+
+
+REM us-east-1  - Virginia
+REM us-west-2  - Oregon
+REM af-south-1 - Africa
+REM eu-north-1 - Estocolmo
+REM sa-east-1  - San Pablo
+
+REM for %%r in (us-east-1 eu-south-1 eu-north-1 sa-east-1 ap-northeast-3) do (
+
 for %%x in (glaciar racer1 racer2 racer3) do (
 
-    @echo on
+    @echo off
 
-    echo:
-    echo:
-    echo: ....[ PROFILE: %%x ]...........................
-    echo:    
+        echo:
+        echo: ....[ PROFILE: %%x ].................................................
 
-    aws ec2 describe-instances --filters Name=instance-state-name,Values=running ^
-        --query "Reservations[].Instances[*].{InstanceType: InstanceType, InstanceId: InstanceId, State: State.Name, Placement: Placement.AvailabilityZone }"  ^
-        --output table ^
-        --profile %%x
+    for %%r in (us-east-1 us-west-2 af-south-1 eu-north-1 sa-east-1) do (
+        
+        echo: ....[ PROFILE: %%x - region: %%r  ].....
+
+        aws ec2 describe-instances --filters Name=instance-state-name,Values=running ^
+            --query "Reservations[].Instances[*].{InstanceType: InstanceType, InstanceId: InstanceId, State: State.Name, Placement: Placement.AvailabilityZone }"  ^
+            --output table ^
+            --region  %%r  ^
+            --profile %%x
+
+        )
 
     @echo off
 
 )
 
 
-echo: para detener algo:
+echo: util algo:
 
 echo: aws ec2 stop-instances --profile profile --instance-ids id-todelete
