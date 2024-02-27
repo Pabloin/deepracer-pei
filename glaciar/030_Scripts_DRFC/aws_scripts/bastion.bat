944f41f1eda89b676539b13dd290185aee6d7700
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-02-27 11:30:00 -0300

    Add Bat Scripts

--- glaciar/030_Scripts_DRFC/aws_scripts/bastion.bat
+++ glaciar/030_Scripts_DRFC/aws_scripts/bastion.bat
@@ -1,3 +1,6 @@
 
+REM El bastion esta en la cuenta EDU (no en luz...)
+REM Paciencia .. no si es que tarda como 5 minutos o es el sg y la IP cambio ...  
+
 ssh -i "borrame-pablo-edu-gmail.pem" ubuntu@ec2-3-219-253-232.compute-1.amazonaws.com
 
