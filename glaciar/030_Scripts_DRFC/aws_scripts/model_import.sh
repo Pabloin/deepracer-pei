3ecbf5d95152b1d2917842fdce3a7cb5657f38cc
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-03-06 18:16:20 -0300

    Pg3-0100w

---
+++ glaciar/030_Scripts_DRFC/aws_scripts/model_import.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Hard de Minio Down ... 
+# Minio up S3 e import
+# Todo eso con un cron cada una hora ... 
+
+MODELO_NAME=Pg3-0100w
+MODELO_ALIAS=Pg3-0100w-R2-Closed
+
+cat << EOM
+
+    aws deepracer import-model 
+        --type REINFORCEMENT_LEARNING 
+        --name ${MODELO_ALIAS}  
+        --model-artifacts-s3-path s3://dr-models-racer2-dots-to-pabloedu-gmail/DR-Qualifier/${MODELO_NAME} 
+        --role-arn arn:aws:iam::590184033458:role/AWSDeepRacerAccessRole 
+        --profile  racer2
+
+EOM
+
+read -p "Hacemos Import? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return 1
+
+aws deepracer import-model \
+    --type REINFORCEMENT_LEARNING \
+    --name ${MODELO_ALIAS}  \
+    --model-artifacts-s3-path s3://dr-models-racer2-dots-to-pabloedu-gmail/DR-Qualifier/${MODELO_NAME} \
+    --role-arn arn:aws:iam::590184033458:role/AWSDeepRacerAccessRole \
+    --profile  racer2
+
+
---
+++ glaciar/030_Scripts_DRFC/aws_scripts/model_sp_dw_up.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+MODELO_NAME=Pg3-0100w
+MODELO_ALIAS=Pg3-0100w-R2-Closed
+
+cat << EOM
+
+        mc cp myminio/bucket-models-2024-03/DR-Qualifier/${MODELO_NAME}
+                    /home/ubuntu/MINIO_SYNC/DR-Qualifier/ --recursive
+
+        cd  /home/ubuntu/MINIO_SYNC/DR-Qualifier
+
+        ls -la
+
+        aws s3 sync  /home/ubuntu/MINIO_SYNC/DR-Qualifier/${MODELO_NAME} 
+                     s3://dr-models-racer2-dots-to-pabloedu-gmail/DR-Qualifier/${MODELO_NAME}
+                     --profile racer2
+
+EOM
+
+read -p "Hacemos DW? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return 1
+
+mc cp myminio/bucket-models-2024-03/DR-Qualifier/${MODELO_NAME}  \
+            /home/ubuntu/MINIO_SYNC/DR-Qualifier/ --recursive
+
+cd  /home/ubuntu/MINIO_SYNC/DR-Qualifier
+
+ls -la
+
+
+#----------------
+
+read -p "Hacemos DW & UP? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return 1
+
+
+aws s3 sync  /home/ubuntu/MINIO_SYNC/DR-Qualifier/${MODELO_NAME} \
+        s3://dr-models-racer2-dots-to-pabloedu-gmail/DR-Qualifier/${MODELO_NAME} \
+                --profile racer2
+
+
+
