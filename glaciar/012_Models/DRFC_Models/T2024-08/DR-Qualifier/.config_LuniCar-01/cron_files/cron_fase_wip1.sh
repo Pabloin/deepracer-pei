cbfd762046bbf38858316a6942e100a3d4f5a827
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-08-24 23:52:44 -0300

    Add Crontab

--- glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LuniCar-01/cron_files/cron_fase_wip1.sh
+++ glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LuniCar-01/cron_files/cron_fase_wip1.sh
@@ -19,6 +19,8 @@
 # 02:33 (GMT  0)
 
 MY_TIME=$(date +%Y-%m-%dT%H:%M:%S)
+MY_NAME=$(date +d%dh%H%M)
+
 
 
 echo "${MY_TIME} CRONTAB - FASE WIP 1" >> ~/logs.crontab
@@ -27,10 +29,9 @@ echo "${MY_TIME} CRONTAB - FASE WIP 1" >> ~/logs.crontab
 
 MODEL_FOLDER=LuniCar-01
 # MODEL_NOMBRE=LuniCar-01-N1-Inicial
-MODEL_NOMBRE=LuniCar-01-N1-Wip1
+# MODEL_NOMBRE=LuniCar-01-N1-Wip1
+MODEL_NOMBRE=LuniCar-01-${MY_NAME}
 
-
-# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LuniCar-01/cron_files/cron_fase_wip1.sh
 # 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LuniCar-01/cron_files/cron_fase_wip1.sh
 
 
