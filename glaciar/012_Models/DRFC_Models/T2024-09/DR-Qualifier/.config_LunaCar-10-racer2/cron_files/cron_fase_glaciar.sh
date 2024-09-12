0582d1d43f7c86a2cec9df513078bb5e10f5648c
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-09-12 00:43:42 -0300

    Add cron config

--- glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_glaciar.sh
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_glaciar.sh
@@ -24,8 +24,8 @@ MY_TIME=$(date +%Y-%m-%dT%H:%M:%S)
 MY_NAME=$(date +d%dh%H%M)
 
 
-MODEL_FOLDER=LunaCar-06-glaciar
-MODEL_NOMBRE=LunaCar-06-glaciar-${MY_NAME}
+MODEL_FOLDER=LunaCar-10-glaciar
+MODEL_NOMBRE=LunaCar-10-glaciar-${MY_NAME}
 
 PROFILE=glaciar
 ROLE_ARN=arn:aws:iam::845305768689:role/service-role/AWSDeepRacerAccessRole
@@ -34,8 +34,8 @@ BUCKET_MINIO=dr-models-glaciar-dots-to-pabloedu-gmail
 
 echo "${MY_TIME} CRONTAB - ${PROFILE}" >> ~/logs.crontab
 
-# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-06-glaciar/cron_files/cron_fase_wip1.sh
-# */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-06-glaciar/cron_files/cron_fase_wip1.sh
+# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-10-glaciar/cron_files/cron_fase_wip1.sh
+# */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-10-glaciar/cron_files/cron_fase_wip1.sh
 
 
 cat << EOM >> ~/logs.crontab
--- glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_racer2.sh
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_racer2.sh
@@ -37,6 +37,8 @@ echo "${MY_TIME} CRONTAB - ${PROFILE}" >> ~/logs.crontab
 # 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_wip1.sh
 # */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_wip1.sh
 
+0 * 11,12 9 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-10-racer2/cron_files/cron_fase_racer2.sh
+
 
 cat << EOM >> ~/logs.crontab
 
