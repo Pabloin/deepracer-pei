d40762e5988913acb43cd3330a091cd8a0345fa2
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-09-13 11:57:35 -0300

    LunaCar-11

--- glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-11-racer2/cron_files/cron_glaciar.sh
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-11-racer2/cron_files/cron_glaciar.sh
@@ -34,10 +34,10 @@ BUCKET_MINIO=dr-models-glaciar-dots-to-pabloedu-gmail
 
 echo "${MY_TIME} CRONTAB - ${PROFILE}" >> ~/logs.crontab
 
-# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-11-glaciar/cron_files/cron_fase_wip1.sh
-# */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-11-glaciar/cron_files/cron_fase_wip1.sh
+# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-11-glaciar/cron_files/cron_wip1.sh
+# */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_LunaCar-11-glaciar/cron_files/cron_wip1.sh
 
-# 0 * 11,12 9 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-11-racer2/cron_files/cron_fase_glaciar.sh
+# 0 * 11,12 9 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-11-racer2/cron_files/cron_glaciar.sh
 
 
 cat << EOM >> ~/logs.crontab
