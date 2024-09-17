b08b7568fbe93a40f34ccbc004bd2e1230b38703
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-09-17 19:50:54 -0300

    Commit Luni_car_12

---
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_LunaCar-12-racer2/cron_files/cron_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 23:33 (GMT -3) 
+# 02:33 (GMT  0)
+# d25h0249
+
+MY_TIME=$(date +%Y-%m-%dT%H:%M:%S)
+MY_NAME=$(date +d%dh%H%M)
+
+MODEL_NOMBRE=LunaCar-11-racer2-${MY_NAME}
+
+echo "${MY_TIME} Hola Pablo ${MODEL_NOMBRE}" >> ~/logs.crontab
+
+# 40,50 2 7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-04/DR-Qualifier/.config_LunaCar-12-racer2/cron_files/cron_test.sh
+
+
