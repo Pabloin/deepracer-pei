2da42eeff3e792a09061ed6bbb49f49bbe464a9d
Inchausti <A115106@arg.bbva.com>
2024-09-26 09:44:01 -0300

    Rufus 95

---
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Rufus-95-racer1/cron_files/cron_racer2.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+#--------------------
+#  minute  hour  day_of_month month day_of_week command_to_run
+
+# 10 2,4    7 3 *: ejecuta el comando 10 minutos en las hora (2, 4, 6) - Ed día 7 del mes - en marzo
+    
+# 30 1      7 3 *  comando_inicial   (entrena 1.30)
+# 10 3      7 3 *  comando_wip1
+
+# */45 * 24,25 8 *    Cada 45 minutos del 24 y 25 de agosto ..
+
+# 0 * 11,12 9 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Rufus-95-racer1/cron_files/cron_racer2.sh
+
+# 23:33 (GMT -3) 
+# 02:33 (GMT  0)
+
+MY_TIME=$(date +%Y-%m-%dT%H:%M:%S)
+MY_NAME=$(date +d%dh%H%M)
+
+
+MODEL_FOLDER=Rufus-95-racer1
+MODEL_NOMBRE=Rufus-95-racer1-${MY_NAME}
+
+PROFILE=racer2
+ROLE_ARN=arn:aws:iam::590184033458:role/AWSDeepRacerAccessRole
+BUCKET_AWS=dr-models-racer2-dots-to-pabloedu-gmail
+BUCKET_MINIO=dr-models-racer2-dots-to-pabloedu-gmail
+
+echo "${MY_TIME} CRONTAB - ${PROFILE}" >> ~/logs.crontab
+
+# 10 3      7 3 *  ~/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_Rufus-95-racer1/cron_files/cron_fase_wip1.sh
+# */45 * 27,28 8 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-08/DR-Qualifier/.config_Rufus-95-racer1/cron_files/cron_fase_wip1.sh
+
+0 * 11,12 9 *  /home/ubuntu/deepracer-pei/glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Rufus-95-racer1/cron_files/cron_fase_racer1.sh
+
+
+cat << EOM >> ~/logs.crontab
+
+        CRONTAB_DE: ${MY_TIME}
+
+        mc cp myminio/${BUCKET_MINIO}/DR-Qualifier/${MODEL_FOLDER}
+                        /home/ubuntu/MINIO_SYNC/DR-Qualifier/ --recursive
+
+        cd  /home/ubuntu/MINIO_SYNC/DR-Qualifier
+
+        ls -la
+
+        aws s3 sync  /home/ubuntu/MINIO_SYNC/DR-Qualifier/${MODEL_FOLDER}
+                          s3://${BUCKET_AWS}/DR-Qualifier/${MODEL_FOLDER}
+                          --profile ${PROFILE}
+
+EOM
+
+mc cp myminio/${BUCKET_MINIO}/DR-Qualifier/${MODEL_FOLDER}  \
+            /home/ubuntu/MINIO_SYNC/DR-Qualifier/ --recursive
+
+cd  /home/ubuntu/MINIO_SYNC/DR-Qualifier
+
+ls -la
+
+
+#----------------
+
+aws s3 sync  /home/ubuntu/MINIO_SYNC/DR-Qualifier/${MODEL_FOLDER} \
+             s3://${BUCKET_AWS}/DR-Qualifier/${MODEL_FOLDER} \
+             --profile ${PROFILE}
+
+
+#----------------
+
+cat << EOM >> ~/logs.crontab
+
+        CRONTAB_DE: ${MY_TIME}
+
+        aws deepracer import-model
+                --type REINFORCEMENT_LEARNING
+                --name ${MODEL_NOMBRE} 
+                --model-artifacts-s3-path s3://${BUCKET_AWS}/DR-Qualifier/${MODEL_FOLDER}
+                --role-arn ${ROLE_ARN}
+                --region   us-east-1
+                --profile ${PROFILE}
+
+EOM
+
+
+aws deepracer import-model \
+    --type REINFORCEMENT_LEARNING \
+    --name ${MODEL_NOMBRE}  \
+    --model-artifacts-s3-path s3://${BUCKET_AWS}/DR-Qualifier/${MODEL_FOLDER} \
+    --role-arn ${ROLE_ARN} \
+    --region   us-east-1 \
+    --profile ${PROFILE}
+
+
+
---
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Rufus-95-racer1/custom_files/reward_function.py
@@ -0,0 +1,92 @@
+import math
+
+# LunaCar-06
+class LunaCar:
+
+    def __init__(self):
+        self.proge_prev = 0
+        self.steps_prev = 0
+        self.steps_now = 0
+    
+    def reko_fun(self, params):
+        
+        angle_kar = params['steering_angle']
+        angle_car = abs(angle_kar)
+
+        stp = params['steps']
+        hd = params['heading']
+        wc = params['closest_waypoints']
+        wp = params['waypoints']
+    
+        pp0 = wp[wc[0]]
+        pp1 = wp[wc[1]]
+
+
+        recompensa = 1e-4  # SIN RECOMPENSA
+
+        progre = max(0, params['progress'])
+        
+        if params['is_offtrack'] and (progre != 100):
+            return self.recompensar(recompensa)
+
+        if stp == 1:
+            self.proge_prev = 0
+            self.steps_prev = self.steps_now
+            self.steps_now = 0
+        else:
+            self.steps_now = self.steps_now + 1
+
+        recompensa = progre - self.proge_prev
+        self.proge_prev = progre
+
+        if progre >= 100 and (self.steps_now < self.steps_prev):
+            recompensa = recompensa + 10.0
+
+        # rrkkoo = self.progress_level_eval(stp, progre)
+
+        # recompensa = recompensa + rrkkoo
+
+        g_diff = abs(math.degrees(math.atan2(pp1[1] - pp0[1], pp1[0] - pp0[0])) - hd)
+
+        if g_diff > 180:
+            g_diff = 360 - g_diff
+
+
+        ANGULO_MAX = 25
+        CABEZA_MAX = 15
+
+        if g_diff > ANGULO_MAX or angle_car > CABEZA_MAX:
+            recompensa = recompensa * 0.8
+
+        return self.recompensar(recompensa)
+
+    def recompensar(self, reko):
+        return float(max(1e-4, reko))
+    
+
+    # def progress_level(self, stp, progre, MAX_STEPS):
+    #     if (stp % 25) == 0 and progre > (stp / MAX_STEPS) * 100 :
+    #         return True
+    #     return False
+
+
+    # def progress_level_eval(self, stp, progre):
+
+    #     CONST_SUPERIOR  = 340
+    #     CONST_INFERIOR  = 270   
+    #     CONST_DISTANCIA = CONST_SUPERIOR - CONST_INFERIOR
+
+    #     for i in range(CONST_DISTANCIA, 0, -1):
+    #         VAR_PIVOTE = CONST_SUPERIOR - i
+    #         VAR_REWARD = i
+
+    #         if self.progress_level(stp, progre, VAR_PIVOTE):
+    #             return VAR_REWARD
+        
+    #     return  0.0
+
+dr = LunaCar()
+
+def reward_function(params):
+    return dr.reko_fun(params)
+
