285c18d9702af67f85bb56c3027bfa5e4f78ec9e
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2024-09-30 00:18:09 -0300

    Commit Luna 52 biz

---
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Luna-52-racer1/README.md
@@ -0,0 +1,54 @@
+# deepracer-pei
+
+
+cat << EOF > /home/ubuntu/start.sh
+source /home/ubuntu/deepracer-for-cloud/bin/activate.sh
+dr-stop-training
+dr-increment-training -f
+dr-start-training -q -w
+EOF
+
+---------------------
+Duda Luna51
+
+IP 15.228.85.141 
+15.228.87.123
+http://a795554ef77e:8888/lab?token=e61bf1befaebfbda88aee31814c90763e1e3745c34cef807
+
+http://15.228.85.141:8888/lab?token=e61bf1befaebfbda88aee31814c90763e1e3745c34cef807
+
+
+REM 15.228.87.123
+ssh -i "racer1-sanpablo.pem" -o ServerAliveInterval=60 -o ServerAliveCountMax=10 ubuntu@ec2-15-228-87-123.sa-east-1.compute.amazonaws.com
+
+
+---------------------
+
+Luna-52-racer1-1-ckp533-last   00:35.870
+Luna-52-racer1-1-ckp334-best2  00:39.733   00:33.200
+    Y en eval están en 27 (2 off tracks - 4 segundos - 23 segundos .. )
+
+    Observar que se la pone a 12.36 ms por segundo ..
+
+    - Tiene problemas en la Zona de las curvas
+        a) Waypoints 130 a 180
+        b) a qué velocidad va en esos waypoints ?
+            De acuerdo al video .. 
+
+
+---------------------
+
+american hills speedway	American Hills Speedway	2020-11-10T00:00:00Z	Austin.npy	
+
+Luna-52-racer1
+
+PREFIX='model-name'   # Name of the model, without trailing '/'
+BUCKET='bucket'       # Bucket name is default 'bucket' when training locally
+PROFILE=None          # The credentials profile in .aws - 'minio' for local training
+S3_ENDPOINT_URL=None  # Endpoint URL: None for AWS S3, 'http://minio:9000' for local training
+
+
+PREFIX='DR-Qualifier/Luna-52-racer1'
+BUCKET='dr-models-racer1'
+S3_ENDPOINT_URL='http://minio:9000'
+PROFILE=None   
---
+++ glaciar/012_Models/DRFC_Models/T2024-09/DR-Qualifier/.config_Luna-52-racer1/custom_files/reward_function.py
@@ -0,0 +1,127 @@
+import math
+
+# LunaCar-06
+class LunaCar:
+
+    def __init__(self):
+        self.proge_prev = 0
+        self.steps_prev = 0
+        self.steps_now = 0
+    
+    def reko_fun(self, params):
+        
+        angle_kar = params['steering_angle']
+        angle_car = abs(angle_kar)
+
+        stp = params['steps']
+        hd = params['heading']
+        wc = params['closest_waypoints']
+        wp = params['waypoints']
+        speed = params['speed']
+    
+        pp0 = wp[wc[0]]
+        pp1 = wp[wc[1]]
+
+
+        recompensa = 1e-4  # SIN RECOMPENSA
+
+        progre = max(0, params['progress'])
+        
+        if params['is_offtrack'] and (progre != 100):
+            return self.recompensar(recompensa)
+
+        if stp == 1:
+            self.proge_prev = 0
+            self.steps_prev = self.steps_now
+            self.steps_now = 0
+        else:
+            self.steps_now = self.steps_now + 1
+
+        recompensa = progre - self.proge_prev
+        self.proge_prev = progre
+
+        if progre >= 100 and (self.steps_now < self.steps_prev):
+            recompensa = recompensa + 10.0
+
+        # rrkkoo = self.progress_level_eval(stp, progre)
+
+        # recompensa = recompensa + rrkkoo
+
+        # g_diff = abs(math.degrees(math.atan2(pp1[1] - pp0[1], pp1[0] - pp0[0])) - hd)
+
+        # if g_diff > 180:
+        #     g_diff = 360 - g_diff
+
+
+        # ANGULO_MAX = 25
+        # CABEZA_MAX = 15
+
+        # if g_diff > ANGULO_MAX or angle_car > CABEZA_MAX:
+        #     recompensa = recompensa * 0.8
+
+
+        # WAYPOINT IF RECTA 1
+
+        # RECTA1 - 100 - 115
+        # RECTA2 - 215 - 223
+        # RECTA3 -  25 -  40
+        # RECTA4 -  55 -  70
+
+        if pp1 in range(100, 115+1) and speed >= 3.2:
+            recompensa *= 1.2
+
+        if pp1 in range(215, 223+1) and speed >= 3.2:
+            recompensa *= 1.2
+
+        if pp1 in range( 25, 40+1)  and speed >= 3.2:
+            recompensa *= 1.2
+
+        if pp1 in range( 55, 70+1)  and speed >= 3.0:
+            recompensa *= 1.2
+
+        # CURVA1 -   10 -  20
+        # CURVA2 -   80 -  90
+        # CURVA3 -  190 - 205
+
+        if pp1 in range( 10, 20+1)  and speed >= 1.8:
+            recompensa *= 1.2
+
+        if pp1 in range( 80, 90+1)  and speed >= 1.8:
+            recompensa *= 1.2
+
+        if pp1 in range(190, 205+1)  and speed >= 1.8:
+            recompensa *= 1.2
+
+
+        return self.recompensar(recompensa)
+
+    def recompensar(self, reko):
+        return float(max(1e-4, reko))
+    
+
+    # def progress_level(self, stp, progre, MAX_STEPS):
+    #     if (stp % 25) == 0 and progre > (stp / MAX_STEPS) * 100 :
+    #         return True
+    #     return False
+
+
+    # def progress_level_eval(self, stp, progre):
+
+    #     CONST_SUPERIOR  = 340
+    #     CONST_INFERIOR  = 270   
+    #     CONST_DISTANCIA = CONST_SUPERIOR - CONST_INFERIOR
+
+    #     for i in range(CONST_DISTANCIA, 0, -1):
+    #         VAR_PIVOTE = CONST_SUPERIOR - i
+    #         VAR_REWARD = i
+
+    #         if self.progress_level(stp, progre, VAR_PIVOTE):
+    #             return VAR_REWARD
+        
+    #     return  0.0
+
+dr = LunaCar()
+
+def reward_function(params):
+    return dr.reko_fun(params)
+
